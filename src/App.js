6fa2eb0252302049534925592d4e7138ffa2ba6e
cortinabryan <cortinabryan@gmail.com>
2022-11-04 14:10:08 -0700

    added divider and hero component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Navbar from "./comps/Navbar";
+import Divider from "./comps/Divider";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Divider/>
     </div>
   );
 }
---
+++ src/comps/Divider.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Hero from './Hero'
+
+const Divider = () => {
+  return (
+    <div className="flex w-full">
+        <div className="grid h-20 flex-grow card bg-base-300 rounded-box place-items-center"><Hero/></div>
+         <div className="divider divider-horizontal">OR</div>
+        <div className="grid h-20 flex-grow card bg-base-300 rounded-box place-items-center">content</div>
+    </div>
+  )
+}
+
+export default Divider
