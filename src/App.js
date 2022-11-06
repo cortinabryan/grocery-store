1725f0c47134e86d14f79529544ff88b93ae8bcd
cortinabryan <64225493+cortinabryan@users.noreply.github.com>
2022-11-05 22:14:28 -0700

    added hero component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import logo from "./logo.svg";
 import "./App.css";
 import Navbar from "./comps/Navbar";
 import Divider from "./comps/Divider";
+import Hero from "./comps/Hero";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <Divider/>
+      <Hero />
     </div>
   );
 }
