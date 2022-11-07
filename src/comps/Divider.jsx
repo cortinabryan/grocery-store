4cd3beac236fb434c1ced9a537d63bf1f1b0aeb2
cortinabryan <64225493+cortinabryan@users.noreply.github.com>
2022-11-07 14:01:34 -0800

    added more images, Updated divider component

--- src/comps/Divider.jsx
+++ src/comps/Divider.jsx
@@ -1,18 +1,21 @@
 import React from "react";
 import Hero from "./Hero";
-import Pig from "../images/pig.avif";
+import Vegetables from "../images/vegetables2.avif";
+import Groceries from "../images/groceries.avif";
 
 const Divider = () => {
   return (
     <div className="flex w-full">
-      <div className="container divider-1 grid h-20 flex-grow card bg-base-300 rounded-box place-items-center my-4 mx-4">
-        <img className="divider-1-inner" src={Pig} alt="" />
-        content
-        <div></div>
+      <div className="divider-1 grid h-20 flex-grow card bg-base-300 rounded-box place-items-center my-4 mx-4">
+        <img className="divider-img" src={Groceries} alt="" />
+        <h2>Hand picked ingredients fresh off the farm and stuff</h2>
+        <button>Click me</button>
       </div>
       <div className="divider divider-horizontal">OR</div>
-      <div className="grid h-20 flex-grow card bg-base-300 rounded-box place-items-center my-4 mx-4">
-        content
+      <div className="divider-2 grid h-20 flex-grow card bg-base-300 rounded-box place-items-center my-4 mx-4">
+        <img className="divider-img" src={Vegetables} alt="" />
+        <h2>Fresh veggies 100% Locally grown from mexico</h2>
+        <button>Click me</button>
       </div>
     </div>
   );
