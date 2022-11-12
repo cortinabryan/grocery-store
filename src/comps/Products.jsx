e505f0682b5480a8140c3ad002ed9244ba59ebe5
cortinabryan <64225493+cortinabryan@users.noreply.github.com>
2022-11-12 10:24:17 -0800

    justified products contents, added and modified more product cards

--- src/comps/Products.jsx
+++ src/comps/Products.jsx
@@ -3,11 +3,17 @@ import ProductsCard from "./ProductsCard";
 
 const Products = () => {
   return (
-    <div className="products mt-5">
+    <div className="products mt-20">
       <h2 className="mb-5 text-5xl font-bold text-center text-primary">
         Products
       </h2>
       <div className="products-inner grid grid-cols-3 bg-base-300">
+        <ProductsCard />
+        <ProductsCard />
+        <ProductsCard />
+        <ProductsCard />
+        <ProductsCard />
+        <ProductsCard />
         <ProductsCard />
         <ProductsCard />
         <ProductsCard />
--- src/comps/ProductsCard.jsx
+++ src/comps/ProductsCard.jsx
@@ -2,19 +2,19 @@ import React from "react";
 
 const ProductsCard = () => {
   return (
-    <div className="card w-96 bg-base-100 shadow-xl">
+    <div className="products-card card w-96 bg-base-100 shadow-xl justify-self-center my-20">
       <figure>
         <img src="https://placeimg.com/400/225/arch" alt="Shoes" />
       </figure>
       <div className="card-body">
         <h2 className="card-title">
-          Shoes!
+          Mangoes
           <div className="badge badge-secondary">20% Off</div>
         </h2>
         <p>If a dog chews shoes whose shoes does he choose?</p>
-        <div className="card-actions justify-end">
-          <div className="badge badge-outline">Fashion</div>
-          <div className="badge badge-outline">Products</div>
+        <div className="card-actions justify-center">
+          <div className="badge badge-outline">Produce</div>
+          <div className="badge badge-outline">Organic</div>
         </div>
       </div>
     </div>
