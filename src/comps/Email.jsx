a93bbecb92c5f6df6d8d392a5695ed4690f81689
cortinabryan <64225493+cortinabryan@users.noreply.github.com>
2022-11-13 10:48:20 -0800

    Modified email component, added social media section, installed react icons

--- src/comps/Email.jsx
+++ src/comps/Email.jsx
@@ -1,25 +1,50 @@
 import React from "react";
+import { MdMailOutline } from "react-icons/md";
+import { RiFacebookBoxFill } from "react-icons/ri";
+import { AiOutlineInstagram } from "react-icons/ai";
+import { TfiTwitterAlt } from "react-icons/tfi";
+import { AiFillYoutube } from "react-icons/ai";
 
 const Email = () => {
   return (
-    <div className="flex flex-col w-full lg:flex-row">
+    <div className="flex flex-col w-full lg:flex-row my-20 justify-center">
       <div
-        className="grid flex-grow h-48 card bg-base-300 rounded-box place-items-center bg-center"
-        style={{
-          backgroundImage: `url("https://images.unsplash.com/photo-1607863680151-1da3e60691bb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80	")`,
-        }}
+        className="grid basis-2/5 h-72 card bg-base-300 rounded-box place-items-center bg-center"
+        // style={{
+        //   backgroundImage: `url("https://images.unsplash.com/photo-1607863680151-1da3e60691bb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80	")`,
+        // }}
       >
-        <h2 className="mb-5 text-5xl font-bold text-white">
+        <MdMailOutline size={50} className="text-white mt-5" />
+        <h2 className="text-3xl font-bold text-white">
           Sign up to our Email List to get more Deals!
         </h2>
-        <p className="">
-          When you sign up to our mailing list you get email notifcations of
-          discounts, promos and latest news. We will not spam you!
+        <p className="text-center max-w-xl">
+          When you sign up to our mailing list you agree to get email
+          notifications about discounts, weekly promos, latest news, etc... We
+          will not spam you! 😆
         </p>
+        <div className="form-control">
+          <label className="input-group">
+            <input
+              type="text"
+              placeholder="Email"
+              className="input input-bordered"
+            />
+            <button className="btn btn-primary mb-5">Sign Up</button>
+          </label>
+        </div>
       </div>
-      <div className="divider lg:divider-horizontal">OR</div>
-      <div className="grid flex-grow h-32 card bg-base-300 rounded-box place-items-center">
-        content
+      <div className="divider lg:divider-horizontal"></div>
+      <div className="grid basis-2/5 h-72 card bg-base-300 rounded-box place-items-center">
+        <h2 className="text-3xl font-bold text-white">
+          Follow us on Social Media!
+        </h2>
+        <div className="flex flex-row self-start space-x-10">
+          <RiFacebookBoxFill size={50} className="cursor-pointer" />
+          <AiOutlineInstagram size={50} className="cursor-pointer" />
+          <TfiTwitterAlt size={50} className="cursor-pointer" />
+          <AiFillYoutube size={50} className="cursor-pointer" />
+        </div>
       </div>
     </div>
   );
