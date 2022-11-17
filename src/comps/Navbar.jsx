8f8ed87b30a00623ac72f89821d597d23cacd838
cortinabryan <cortinabryan@gmail.com>
2022-11-16 17:54:02 -0800

    Added total price functionality

--- src/comps/Navbar.jsx
+++ src/comps/Navbar.jsx
@@ -3,6 +3,11 @@ import Logo from "../images/logo-transparent.png";
 
 const Navbar = (props) => {
   const {cartItems, onAdd, onRemove} = props;
+  const itemsPrice = cartItems.reduce((a,c) => a + c.price * c.qty, 0);
+  const taxPrice = itemsPrice * 0.14;
+  const shippingPrice = itemsPrice > 2000 ? 0 : 50;
+  const totalPrice = itemsPrice + taxPrice + shippingPrice; 
+
   return (
     <div className="navbar bg-base-300">
       <div className="flex-1">
@@ -56,6 +61,27 @@ const Navbar = (props) => {
                     <div className="col-2 text-right text-lg">{item.qty} x ${item.price.toFixed(2)}</div>
                 </div>
               ))}
+              {cartItems.length !== 0 && (
+                <>
+                <hr></hr>
+                <div className="row">
+                  <div className="col-2">Items Price</div>
+                  <div className="col-1 text-right">${itemsPrice.toFixed(2)}</div>
+                </div>
+                <div className="row">
+                  <div className="col-2">Tax Price</div>
+                  <div className="col-1 text-right">${taxPrice.toFixed(2)}</div>
+                </div>
+                <div className="row">
+                  <div className="col-2">Shipping Price</div>
+                  <div className="col-1 text-right">${shippingPrice.toFixed(2)}</div>
+                </div>
+                <div className="row">
+                  <div className="col-2"><strong>Total Price</strong></div>
+                  <div className="col-1 text-right"><strong>${totalPrice.toFixed(2)}</strong></div>
+                </div>
+                </>
+              )}
               <span className="font-bold text-lg">0 Items</span>
               <span className="text-info">Subtotal: $0</span>
               <div className="card-actions">
